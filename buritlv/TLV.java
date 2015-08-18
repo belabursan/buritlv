ad810630e15c9aaf393e4a2cccd1e1d02d88819a
Bela Bursan <bela.bursan@hiq.se>
2015-08-18 22:21:25 +0200

    continued with TLV implementation

--- buritlv/TLV.java
+++ buritlv/TLV.java
@@ -23,6 +23,7 @@
  */
 package buritlv;
 
+import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.Charset;
 
@@ -34,22 +35,24 @@ public final class TLV {
 
     private static final int MAX_VALUE_LENGTH = 65535;//32767*2;
     private static final int MAX_TAG_LENGTH = 127;//7F;
-    
+
     private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
-    
+
     private static final String errorGreater = String.format("Tag value greater then %s", MAX_TAG_LENGTH);
     private static final String errorNegative = "Tag value cannot be negative";
     private static final String errorTooLong = String.format("Value length greater then %s", MAX_VALUE_LENGTH);
     private static final String errorUnsupportedChild = "Cannot add child to PDO";
-    
+    private static final String tab = "    ";
+
     private static final ByteOrder byteOrder = java.nio.ByteOrder.nativeOrder();
-    
+
     public static final boolean PDO = false;
     public static final boolean CDO = true;
 
     private int tag;
     private int length;
     private byte[] value;
+    private int level;
 
     private TLV sibling;
     private TLV child;
@@ -80,31 +83,71 @@ public final class TLV {
         this.tag = tag;
         this.value = value;
         this.length = 0;
+        this.level = 0;
         this.sibling = null;
         this.child = null;
     }
-    
-    public byte[] toByteArray(){
-        byte[] b = new byte[3 + ( value == null? 0 : value.length)];
-        setFirstByte(tag, isCdo, b);
-        setLengthAndValue(value, b);
+
+    public byte[] toByteArray() {
+        ByteBuffer buffer = ByteBuffer.allocate(3 + (value == null ? 0 : value.length));
+        setFirstByte(tag, isCdo, buffer);
+        setLengthAndValue(value, buffer);
+
+        return buffer.array();
+    }
+
+    public byte[] toBytes() {
+        int totalLength = getByteLength();
+        ByteBuffer buffer = ByteBuffer.allocate(totalLength);
         
-        return b;
+//todo fix bytes
+        return buffer.array();
     }
     
     /**
+     * Returns the length of this TLV (value + TLV data)
+     * @return the length of the value + 3 or just 3 if it is a CDO
+     */
+    public int getLength(){
+        int len = 3;
+        if (this.value != null) {
+            len += this.value.length;
+        }
+        return len;
+    }
+
+    /**
+     * Returns the total length of this TLV (this TLV + child and sibling lengths)
+     * @return length of this TLV inclusive the length of the underlying TLV's
+     */
+    public int getByteLength() {
+        this.length = getLength();
+        if (this.child != null) {
+            this.length += this.child.getByteLength();
+        }
+        int len = this.length;
+        if (this.sibling != null) {
+            len += this.sibling.getByteLength();
+        }
+
+        return len;
+    }
+
+    /**
      * Creates a new TLV of CDO type with tag number
+     *
      * @param tag tag to set
      * @return new TLV of CDO type
      * @throws IllegalArgumentException if tag or value length is greater then
      * allowed
      */
     public static TLV newCDO(int tag) throws IllegalArgumentException {
-        return new TLV(tag, null, true);
+        return new TLV(tag, null, CDO);
     }
-    
+
     /**
      * Creates a new TLV of PDO type with tag number and value
+     *
      * @param tag tag number to set
      * @param value byte array or null to set
      * @return new TLV of PDO type
@@ -112,11 +155,12 @@ public final class TLV {
      * allowed
      */
     public static TLV newPDO(int tag, byte[] value) throws IllegalArgumentException {
-        return new TLV(tag, value, false);
+        return new TLV(tag, value, PDO);
     }
-    
+
     /**
      * Creates a new TLV of PDO type with tag number and value
+     *
      * @param tag tag number to set
      * @param value string or null to set
      * @return new TLV of PDO type
@@ -126,171 +170,214 @@ public final class TLV {
     public static TLV newPDO(int tag, String value) throws IllegalArgumentException {
         return newPDO(tag, encodeUTF8(value));
     }
-    
+
     /**
      * Getter for isCDO
+     *
      * @return true if the TLV is PDO, false otherwise
      */
-    public boolean isCDO(){
+    public boolean isCDO() {
         return this.isCdo;
     }
-    
+
     /**
      * Getter for tag
-     * @return the tag number, positive number between 0 and 127
-     * (inclusive 0 and 127)
+     *
+     * @return the tag number, positive number between 0 and 127 (inclusive 0
+     * and 127)
      */
-    public int getTag(){
+    public int getTag() {
         return this.tag;
     }
-    
+
     /**
      * Getter for value
+     *
      * @return byte array or null
      */
-    public byte[] getValue(){
+    public byte[] getValue() {
         return this.value;
     }
 
     /**
      * Getter for child
+     *
      * @return TLV object corresponding the direct child
      */
-    public TLV getChild(){
+    public TLV getChild() {
         return this.child;
     }
-    
+
     /**
      * Getter for sibling
+     *
      * @return TLV object corresponding the direct "sibling"
      */
-    public TLV getSibling(){
+    public TLV getSibling() {
         return this.sibling;
     }
-    
+
     /**
      * Returns the last child or null if this hasn't any child
+     *
      * @return Last child as TLV object or null
      */
-    public TLV getLastChild(){
-        if(this.child == null) {
+    public TLV getLastChild() {
+        if (this.child == null) {
             return null;
         }
         return child.getLastSibling();
     }
-    
+
     /**
-     * Returns the last TLV object in the "sibling" chain
-     * If the sibling of the object is null, then this object is returned
+     * Returns the last TLV object in the "sibling" chain If the sibling of the
+     * object is null, then this object is returned
+     *
      * @return TLV object
      */
-    public TLV getLastSibling(){
-        if(sibling == null) {
+    public TLV getLastSibling() {
+        if (sibling == null) {
             return this;
         }
         return sibling.getLastSibling();
-     }
-    
+    }
+
     /**
-     * Adds a child to this TLV object
-     * Current child with siblings will be replaced
+     * Adds a child to this TLV object Current child with siblings will be
+     * replaced
+     *
      * @param tlv TLV object to add as first child
      * @return the currently added TLV object
      * @throws UnsupportedOperationException when trying to add child to a PDO
      */
-    public TLV addChild(TLV tlv) throws UnsupportedOperationException {
-        if(!this.isCdo){
+    public TLV setChild(TLV tlv) throws UnsupportedOperationException {
+        if (!this.isCdo) {
             throw new UnsupportedOperationException(errorUnsupportedChild);
         }
         this.child = tlv;
+        this.child.level = this.level + 1;
         return this.child;
     }
-    
+
     /**
      * Adds a child as last child to this TLV object
+     *
      * @param tlv TLV to add as last child
      * @return the currently added TLV object
      * @throws UnsupportedOperationException when trying to add child to a PDO
      */
-    public TLV addChildLast(TLV tlv) throws UnsupportedOperationException {
-        if(this.child == null) {
-            return addChild(tlv);
+    public TLV appendChild(TLV tlv) throws UnsupportedOperationException {
+        if (this.child == null) {
+            return setChild(tlv);
         }
-        return this.child.addSiblingLast(tlv);
+        return this.child.appendSibbling(tlv);
     }
-    
+
     /**
-     * Adds a sibling to this TLV object
-     * Current sibling, if any, will be replaced
+     * Adds a sibling to this TLV object Current sibling, if any, will be
+     * replaced
+     *
      * @param tlv TLV object to add as first sibling
      * @return the currently added TLV object
      */
-    public TLV addSibling(TLV tlv){
+    public TLV setSibbling(TLV tlv) {
         this.sibling = tlv;
+        this.sibling.level = this.level + 1;
         return this.sibling;
     }
-    
+
     /**
      * Adds a sibling as last sibling to this TLV object
+     *
      * @param tlv TLV object to add as last sibling
      * @return the currently added TLV object
      */
-    public TLV addSiblingLast(TLV tlv){
-        if(this.sibling == null) {
-            this.sibling = tlv;
-            return this.sibling;
+    public TLV appendSibbling(TLV tlv) {
+        if (this.sibling == null) {
+            return this.setSibbling(tlv);
         }
-        return this.sibling.addSiblingLast(tlv);
+        return this.sibling.appendSibbling(tlv);
+    }
+    
+    public String toStringAll(){
+        StringBuilder sb = new StringBuilder();
+        String t ="";
+        for (int i = 0; i < level; i++) {
+            t += tab;
+        }
+        sb.append(t).append(toString());
+        if(child != null){
+            sb.append(child.toStringAll());
+        }
+        if(sibling != null){
+            sb.append(sibling.toStringAll());
+        }
+        return sb.toString();
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        
+        if(isCdo) sb.append("CDO-");
+        else sb.append("PDO-");
+        sb.append(String.valueOf(tag));
+        sb.append("(");
+        if(value != null) sb.append(decodeUTF8(value));
+        sb.append(")");
+        sb.trimToSize();
+        return sb.toString();
     }
     ///////////////////////////////////////////////////////////////////////////
 
-     /**
+    /**
      * Converts a a byte array to UTF-8 string
      *
      * @param bytes byte array to convert
-     * @return converted string or null if argument is null
-     * if the byte array is not null but has zero length an empty string 
-     * will be returned
+     * @return converted string or null if argument is null if the byte array is
+     * not null but has zero length an empty string will be returned
      */
     public static String decodeUTF8(byte[] bytes) {
-        if(bytes == null) return null;
+        if (bytes == null) {
+            return null;
+        }
         return new String(bytes, UTF8_CHARSET);
     }
 
-     /**
+    /**
      * Converts a UTF-8 string to a byte array
      *
      * @param value string to convert
-     * @return byte array
-     *          or null if argument is null
-     *          or empty array if string is empty
+     * @return byte array or null if argument is null or empty array if string
+     * is empty
      */
     public static byte[] encodeUTF8(String value) {
-        if (value == null) return null;
+        if (value == null) {
+            return null;
+        }
         return value.getBytes(UTF8_CHARSET);
     }
 
-    private static void setFirstByte(int tag, boolean cdo, byte[] array) {
-        if(cdo) {
-            array[0] = (byte)(tag | 0x00000080);
-        } else{
-            array[0] = (byte)(tag & 0x0000007F);
+    private static void setFirstByte(int tag, boolean cdo, ByteBuffer buffer) {
+        if (cdo) {
+            buffer.put((byte) (tag | 0x00000080));
+        } else {
+            buffer.put((byte) (tag & 0x0000007F));
         }
     }
 
-    private void setLengthAndValue(byte[] value, byte[] b) {
+    private void setLengthAndValue(byte[] value, ByteBuffer buffer) {
         if (value == null || value.length == 0) {
-            b[1] = 0x00;
-            b[2] = 0x00;
-        }else {
-            if(byteOrder == ByteOrder.BIG_ENDIAN){
-                b[1] = (byte)((value.length >> 8) & 0xFF);
-                b[2] = (byte)(value.length & 0xFF);
-            }else{
-                b[1] = (byte)(0x000000FF & value.length);
-                b[2] = (byte)((value.length >> 8) & 0xff);
+            buffer.put((byte)0x00).put((byte)0x00);
+        } else {
+            if (byteOrder == ByteOrder.BIG_ENDIAN) {
+                buffer.put((byte) ((value.length >> 8) & 0xFF));
+                buffer.put((byte) (value.length & 0xFF));
+            } else {
+                buffer.put((byte) (0x000000FF & value.length));
+                buffer.put((byte) ((value.length >> 8) & 0xff));
             }
-            System.arraycopy(value, 0, b, 3, value.length);
+            buffer.put(value);
         }
     }
 }
