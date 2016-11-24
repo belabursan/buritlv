8c1ca1e89b950867979096bf765bb3a882fc2036
Bela Bursan <bela@axis.com>
2016-11-24 16:08:52 +0100

    tmp commit

--- ctlv/TLV.c
+++ ctlv/TLV.c
@@ -2,10 +2,12 @@
 #include "dbg.h"
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
+
 
 static struct TLV* tlv_new();
 static void tlv_reset(struct TLV *tlv);
-
+static int tlv_as_bytes(struct TLV *self, uint8_t **bytes, uint32_t *length);
 
 /////////////////////////////////////////////////////////////////////////////
 
@@ -14,8 +16,7 @@ static void tlv_reset(struct TLV *tlv);
  * Allocates memory for the struct
  * @return pointer to the struct or NULL if allocation failed
  */
-static struct TLV* tlv_new()
-{
+static struct TLV* tlv_new() {
     struct TLV *tlv;
 
     tlv = (struct TLV*) malloc(sizeof (struct TLV));
@@ -36,12 +37,11 @@ static struct TLV* tlv_new()
  * All members will be set to defaults like NULL and 0
  * @param tlv struct TLV to reset
  */
-static void tlv_reset(struct TLV *tlv)
-{
+static void tlv_reset(struct TLV *tlv) {
     tlv->child = NULL;
     tlv->next = NULL;
     tlv->length = 0;
-    tlv->isCdo = 0;
+    tlv->tlvform = 0;
     tlv->tag = 0;
     tlv->value = NULL;
     tlv->level = 0;
@@ -50,8 +50,7 @@ static void tlv_reset(struct TLV *tlv)
 /**
  * @brief Prints the library version to the standard out
  */
-void tlv_version()
-{
+void tlv_version() {
     printf("TLV version: v%s\n", TLV_VERSION);
 }
 
@@ -60,8 +59,7 @@ void tlv_version()
  * Also deletes the value, child and next structs recursively
  * @param tlv struct to delete
  */
-void tlv_free(struct TLV **tlv)
-{
+void tlv_free(struct TLV **tlv) {
     if (tlv != NULL && *tlv != NULL) {
         dbg("tlv_free: deleting tlv with tag %u\n", (*tlv)->tag);
         tlv_free(&(*tlv)->next);
@@ -88,15 +86,14 @@ void tlv_free(struct TLV **tlv)
  * @param tag the tag of the tlv object
  * @return the tlv object or NULL
  */
-struct TLV *tlv_new_cdo(const uint32_t tag)
-{
+struct TLV *tlv_new_cdo(const uint32_t tag) {
     struct TLV *tlv;
-    
+
     if ((tlv = tlv_new()) == NULL) {
         error("tlv_new_cdo: ERROR - Failed to create CDO");
         return NULL;
     }
-    tlv->isCdo = TLV_CDO;
+    tlv->tlvform = TLV_CDO;
     tlv->tag = tag;
 
     return tlv;
@@ -109,16 +106,15 @@ struct TLV *tlv_new_cdo(const uint32_t tag)
  * @param value pointer data to set
  * @return the tlv object or NULL
  */
-struct TLV *tlv_new_pdo(const uint32_t tag, const uint32_t length, uint8_t *value)
-{
+struct TLV *tlv_new_pdo(const uint32_t tag, const uint32_t length, uint8_t *value) {
     struct TLV *tlv;
-    
+
     if ((tlv = tlv_new()) == NULL) {
         error("tlv_new_pdo: ERROR - Failed to create for PDO");
         return NULL;
     }
 
-    tlv->isCdo = TLV_PDO;
+    tlv->tlvform = TLV_PDO;
     tlv->tag = tag;
     tlv->length = length;
     tlv->value = value;
@@ -132,8 +128,7 @@ struct TLV *tlv_new_pdo(const uint32_t tag, const uint32_t length, uint8_t *valu
  * @param next tlv object to append
  * @return pointer to "next" or NULL if next or self is null
  */
-struct TLV* tlv_append_next(struct TLV *self, struct TLV *next)
-{
+struct TLV* tlv_append_next(struct TLV *self, struct TLV *next) {
     if (self && next) {
         struct TLV *tmp = self;
 
@@ -156,10 +151,9 @@ struct TLV* tlv_append_next(struct TLV *self, struct TLV *next)
  * @param child tlv object to append
  * @return pointer to "child" or NULL if next or self is null or self isn't CDO
  */
-struct TLV* tlv_append_child(struct TLV *self, struct TLV *child)
-{
+struct TLV* tlv_append_child(struct TLV *self, struct TLV *child) {
     if (self && child) {
-        if (self->isCdo != TLV_CDO) {
+        if (self->tlvform != TLV_CDO) {
             dbg("tlv_append_child: not possible to append child to a PDO");
             return NULL;
         }
@@ -183,18 +177,17 @@ struct TLV* tlv_append_child(struct TLV *self, struct TLV *child)
  * @param next TLV object to be set as sibling
  * @return the sibling or NULL
  */
-struct TLV *tlv_set_next(struct TLV *self, struct TLV *next)
-{
+struct TLV *tlv_set_next(struct TLV *self, struct TLV *next) {
     if (self && next) {
-        if(self->next != NULL){
+        if (self->next != NULL) {
             tlv_free(&self->next);
         }
         next->level = self->level;
         self->next = next;
-        
+
         return next;
     }
-    
+
     dbg("tlv_set_next: self or next is null");
     return NULL;
 }
@@ -205,10 +198,9 @@ struct TLV *tlv_set_next(struct TLV *self, struct TLV *next)
  * @param child TLV object to be set as child
  * @return returns the child element or NULL
  */
-struct TLV *tlv_set_child(struct TLV *self, struct TLV *child)
-{
+struct TLV *tlv_set_child(struct TLV *self, struct TLV *child) {
     if (self && child) {
-        if (self->isCdo != TLV_CDO) {
+        if (self->tlvform != TLV_CDO) {
             dbg("tlv_set_child: not possible to append child to a PDO");
             return NULL;
         }
@@ -220,7 +212,7 @@ struct TLV *tlv_set_child(struct TLV *self, struct TLV *child)
 
         return child;
     }
-    
+
     dbg("tlv_set_child: self or child is null");
     return NULL;
 }
@@ -231,18 +223,57 @@ struct TLV *tlv_set_child(struct TLV *self, struct TLV *child)
  * @param tag tag of the TLV to search for
  * @return the requested TLV or NULL if not found 
  */
-struct TLV *tlv_find_by_tag(const struct TLV *self, const int32_t tag)
-{
+struct TLV *tlv_find_by_tag(const struct TLV *self, const int32_t tag) {
     struct TLV *tlv = NULL;
-    if(self){
-        if(self->tag == tag){
-            tlv = (struct TLV*)self;
-        } else {        
-            if((tlv = tlv_find_by_tag(self->next, tag)) == NULL){
+    if (self) {
+        if (self->tag == tag) {
+            tlv = (struct TLV*) self;
+        } else {
+            if ((tlv = tlv_find_by_tag(self->next, tag)) == NULL) {
                 tlv = tlv_find_by_tag(self->child, tag);
             }
         }
     }
-    
+
     return tlv;
+}
+
+/**
+ * @brief Converts the TLV object and all it's child to an byte array
+ * @param self [in] TLV object to convert
+ * @param bytes [out] byte array representing the TLV object
+ * @param length [out] the length of the byte array
+ * @return 0 if successful, negative number otherwise
+ */
+int tlv_to_byte_array(struct TLV *self, uint8_t **bytes, uint32_t *length) {
+    struct TLV *tmp = self->child;
+    if (tmp) {
+        //...
+        tmp = tmp->next;
+    }
+
+    return tlv_as_bytes(self, bytes, length);
+}
+
+static int tlv_as_bytes(struct TLV *self, uint8_t **bytes, uint32_t *length)
+{
+    uint8_t noofTagBytes;
+    uint8_t noofLenBytes;
+    uint8_t firstByte = 0;
+    
+    firstByte = self->tlvclass & 0xC0; //set class
+    firstByte |= self->tlvform & 0x20;
+    
+    if(self->tag < 31){
+        // tag lower then 31, there is place in the first byte, set it
+        firstByte |= (uint8_t)(self->tag & 0x1F);
+        noofTagBytes = 0;
+    } else {
+        firstByte |= 0x1F;        
+        noofTagBytes = (int) floor((log((double)self->tag) / log(2)) / 7) + 1;
+    }
+    
+    
+    
+    return -1;
 }
--- ctlv/TLV.h
+++ ctlv/TLV.h
@@ -1,7 +1,7 @@
 /* 
  * File:   TLV.h
- * Author: Bela Bursan
- *
+ * @brief TLV class, represents a tag-length-value objects
+ * @author <a href="mailto:bursan@gmail.com">Bela Bursan</a>
  * Created on October 12, 2016, 9:23 AM
  */
 
@@ -21,22 +21,32 @@ extern "C" {
 
 #include <stdlib.h>
 #include <inttypes.h>
+
     
-    enum TLV_TYPE
+    enum TLV_CLASS
     {
-        TLV_CDO = 0,
-        TLV_PDO = 1
+        CLASS_UNIVERSAL     = 0x00,
+        CLASS_APPLICATION   = 0x40,
+        CLASS_CONTEXT_SPEC  = 0x80,
+        CLASS_PRIVATE       = 0xC0
+    };
+    
+    enum TLV_FORM
+    {
+        TLV_PDO = 0,
+        TLV_CDO = 0x20
     };
 
     struct TLV
     {
-        struct TLV *next;
-        struct TLV *child;
-        uint8_t *value;
-        int32_t tag;
-        uint32_t length;
-        uint8_t isCdo;
-        uint8_t level;
+        struct TLV *next;       // next element on the same level, 
+        struct TLV *child;      // child element, the first if many
+        int32_t tag;            // tag
+        uint32_t length;        // length of the value(in case of pdo, cdo has no data)
+        uint8_t *value;         // data in pdo, null for cdo but can hold child elements when converting to byte array
+        uint8_t tlvclass;   // the class of the tlv
+        uint8_t tlvform;    // boolean indicating if cdo  
+        uint8_t level;      // only used for printing the tlv object
     };
 
     void tlv_version();
@@ -48,6 +58,8 @@ extern "C" {
     struct TLV *tlv_set_next(struct TLV *self, struct TLV *next);
     struct TLV *tlv_set_child(struct TLV *self, struct TLV *child);
     struct TLV *tlv_find_by_tag(const struct TLV *self, const int32_t tag);
+    //
+    int tlv_to_byte_array(struct TLV *self, uint8_t **bytes, uint32_t *length);
     
 
 
@@ -56,5 +68,9 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+
+//g++ -m64   -c -O3 -Werror -s -fPIC  -MMD -MP -MF "build/Release/GNU-Linux/TLV.o.d" -o build/Release/GNU-Linux/TLV.o TLV.c
+//g++ -m64   -c -O3 -Werror -s -fPIC  -MMD -MP -MF "build/Release/GNU-Linux/dbg.o.d" -o build/Release/GNU-Linux/dbg.o dbg.c
+//g++ -m64    -o dist/Release/GNU-Linux/libctlv.so build/Release/GNU-Linux/TLV.o build/Release/GNU-Linux/dbg.o  -shared -fPIC
 
 #endif /* TLV_H */
